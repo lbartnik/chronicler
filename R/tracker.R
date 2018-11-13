139953da9456026a3611f98b6eaab904566e7119
Łukasz A. Bartnik <lbartnik@users.noreply.github.com>
2018-11-13 14:03:57 -0800

    Update docs (#6)
    * minor fix in prompt
    * update docs

--- R/tracker.R
+++ R/tracker.R
@@ -46,7 +46,8 @@ attach_wizard <- function () {
   # TODO check if already attached
 
   showMultipleChoice <- function (title, message, choices) {
-    ans <- rstudioapi::showPrompt(title, paste0(message, '\n', paste(choices, collapse = '\n')))
+    ans <- rstudioapi::showPrompt(title, paste0(message, '\n', paste(choices, collapse = '\n'), '\n'))
+    if (!length(ans)) abort("No answer from user.")
 
     i <- match(ans, choices)
     if (is.na(i)) i <- suppressWarnings(as.integer(ans))
--- R/ui.R
+++ R/ui.R
@@ -1,11 +1,42 @@
-#' Search for artifacts.
+#' Chronicler user interface objects.
 #'
-#' An entry point for artifact search. Exposes an interactive query
-#' mechanism with tab-completion via the `$` (dollar) operator.
+#' @name chronicler-ui
+#' @rdname chronicler-ui
+NULL
+
+#' `artifacts` is the entry point for artifact search. It exposes an
+#' interactive query mechanism with tab-completion via the `$` (dollar)
+#' operator.
 #'
 #' @export
 #' @rdname chronicler-ui
+#'
+#' @examples
+#' \dontrun{
+#' # show summary of all artifacts in repository
+#' artifacts
+#'
+#' # show all R sessions
+#' artifacts$session
+#'
+#' # show artifacts from a given session
+#' artifacts$session$afc61b89
+#'
+#' # show all artifacts with a given name
+#' artifacts$name$input
+#' artifacts$input
+#'
+#' # retrieve a given artifact
+#' artifacts$id$c643b1a$value
+#' }
 artifacts <- NULL
+
+#' `history` gives access to the history of commands stored in the
+#' repositoy along artifacts.
+#'
+#' @export
+#' @rdname chronicler-ui
+history <- NULL
 
 
 #' @rdname session-state
@@ -14,5 +45,6 @@ init_ui <- function (state) {
 
   unlockBinding('artifacts', asNamespace('chronicler'))
   artifacts <<- artifacts_query(state)
+  #history   <<- commits_query(state)
   lockBinding('artifacts', asNamespace('chronicler'))
 }
